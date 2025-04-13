bd7613663f6cd52679b989aa58174ce046024844
m.arfaa <m.arfa@domil.io>
2025-04-13 13:38:34 +0330

    V 1.0.3
    change api address

--- composeApp/src/commonMain/kotlin/env.kt
+++ composeApp/src/commonMain/kotlin/env.kt
@@ -1,2 +1,2 @@
-const val severAddress = "https://rfid-api-dev.avakatan.ir"
+const val severAddress = "DEFAULT_SEVER_ADDRESS"
 const val barcodeScannerPageAddress = "DEFAULT_BARCODE_SCANNER_SERVER_ADDRESS"
