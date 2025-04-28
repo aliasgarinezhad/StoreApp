bf8ac70f46c395d4c08954ff5f24084ea7a2ae06
Behzad Rezvani <b.rezvani@domil.io>
2025-04-28 15:41:37 +0330

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -129,7 +129,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 3
-        versionName = "5.2.8"
+        versionName = "5.2.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
